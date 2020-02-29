fe7ef4c6b54983e30307e7d4bc78e9161e86a67c
Li Alex Zhang <fff008@gmail.com>
2020-02-28 21:38:59 -0800

    refresh

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter,NeuroAnalysis
 
 uname = "Experica"
 pname = "NeuroAnalysis"
-makedocs(sitename=pname,modules=[NeuroAnalysis],
+makedocs(sitename="$pname.jl",modules=[NeuroAnalysis],
     pages=[])
 
 deploydocs(repo = "github.com/$uname/$pname.jl.git")
