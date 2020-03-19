3f5e49474d5520ce2dc1443830b251e1862206bb
Li Alex Zhang <fff008@gmail.com>
2020-03-18 18:59:16 -0700

    1D,2D sin and cas grating and convertion, hartley subspace

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,5 +23,34 @@ ps,f1,f2 = powerspectrum(img,ppd,freqrange=[-6,6])
 eori,esf = freqimagestats(ps,f1,f2)
 @test sf≈esf
 @test ori≈eori
+## hartley space
+# @manipulate for kb in 0:4, ke in 1:5, dk in 0.4:0.2:1, p in 0:0.005:1, shape in [:square, :circle]
+#     plothartleyspace(hartleysubspace(kbegin=kb,kend=ke,dk=dk,phase=p,shape=shape),floor(Int,ke/dk),dk)
+# end
+
+end
+
+@testset "Function" begin
+## 1D grating
+# @manipulate for f in 0:0.1:1, p in 0:0.001:1
+#     plot([x->gratingf(x,f=f,phase=p),x->cas(x,f=f,phase=sin2cas(p))],-2,2,labels=["sin" "cas"])
+# end
+## 2D grating
+# @manipulate for θ in 0:0.01:π, f in 0:0.05:1, sinp in 0:0.001:1, kx in -1:0.05:1,ky in -1:0.05:1, casp in 0:0.001:1
+#     x=y=-2:0.05:2
+#     sg = [gratingf(i,j,θ=θ,f=f,phase=sinp) for j in reverse(y),i in x]
+#     scg = [cas(i,j;sin2cas(θ,f,sinp)...) for j in reverse(y),i in x]
+#     cg = [cas(i,j,kx=kx,ky=ky,phase=casp) for j in reverse(y),i in x]
+#     csg = [gratingf(i,j;cas2sin(kx,ky,casp)...) for j in reverse(y),i in x]
+#
+#     p = plot(layout=(2,3),leg=false,size=(600,400),clims=(-1,1),frame=:none,aspect_ratio=:equal,yflip=true)
+#     heatmap!(p,subplot=1,sg,color=:grays,title="sin")
+#     heatmap!(p,subplot=2,scg,color=:grays,title="sin2cas")
+#     heatmap!(p,subplot=3,sg.-scg,color=:grays,title="sin - sin2cas")
+#     heatmap!(p,subplot=4,cg,color=:grays,title="cas")
+#     heatmap!(p,subplot=5,csg,color=:grays,title="cas2sin")
+#     heatmap!(p,subplot=6,cg.-csg,color=:grays,title="cas - cas2sin")
+#     p
+# end
 
 end
