9f921d9c9cb14aea056250410e224b72a20259fd
Li Alex Zhang <fff008@gmail.com>
2025-10-06 17:46:38 +0800

    directly prepare Experica data version 3

--- src/IO/Experica.jl
+++ src/IO/Experica.jl
@@ -4,7 +4,7 @@ function load(source::Val{Experica},filepath;ext=".yaml",prepare=true)
     fullfilename = filename*ext
     fullfilepath =joinpath(filedir,fullfilename)
     if !isfile(fullfilepath)
-        @warn "No Experica File:    $fullfilename"
+        @error "No Experica File:    $fullfilepath"
         return nothing
     end
 
@@ -13,7 +13,7 @@ function load(source::Val{Experica},filepath;ext=".yaml",prepare=true)
     dataset["ex"] = YAML.load_file(fullfilepath)
     dataset["filename"] = filename
     dataset["filetime"] = ctime(fullfilepath)
-    dataset["datasource"] = Experica
+    dataset["sourceformat"] = Experica
     @info "Reading Experica File:    $fullfilename    Done"
 
     if prepare
@@ -38,11 +38,36 @@ end
 function tryshortenparamname!(d::Dict)
     ks = filter(k->contains(k,'@'),collect(keys(d)))
     sks = map(k->split(k,'@')[1],ks)
-    if allunique(sks)
-        foreach((sk,k)->d[sk]=d[k],sks,ks)
-        foreach(k->delete!(d,k),ks)
-    end
+    cm = countmap(sks)
+    vsks = [sk for (sk,c) in cm if c==1]
+    vks = ks[indexin(vsks,sks)]
+    foreach((sk,k)->d[sk]=d[k],vsks,vks)
+    foreach(k->delete!(d,k),vks)
     d
+end
+function trygetparam(d::Dict,name,fromobject="")
+    ks = filter(k->startswith(k,name) && endswith(k,fromobject),collect(keys(d)))
+    if length(ks)==0
+        @warn "No Matching Param Found: $name*$fromobject"
+        return nothing
+    elseif length(ks)>1
+        @warn "Multiple Matching Param Found, Need To Be More Specific: $ks"
+        return nothing
+    else
+        return d[ks[1]]
+    end
+end
+function trygetscreensize(ps::Dict)
+    d = trygetparam(ps,"ScreenToEye")
+    sh = trygetparam(ps,"ScreenHeight")
+    sr = trygetparam(ps,"ScreenAspect")
+    if isnothing(d) || isnothing(sh) || isnothing(sr)
+        return nothing
+    else
+        h = atand(sh/2,d)*2
+        w = h * sr
+        return (w,h)
+    end
 end
 
 function prepare!(source::Val{Experica},version::Val{2},ex)
@@ -53,7 +78,6 @@ function prepare!(source::Val{Experica},version::Val{2},ex)
     foreach(k->exparam[k]=tryparseparam(exparam[k]), keys(exparam))
     cond = ex["Cond"]
     foreach(k->cond[k]=tryparseparam.(cond[k]), keys(cond))
-    ct = ex["CondTest"]
 end
 
 function prepare!(source::Val{Experica},version::Val{3},ex)
@@ -64,5 +88,39 @@ function prepare!(source::Val{Experica},version::Val{3},ex)
     foreach(k->extparam[k]=tryparseparam(extparam[k]), keys(extparam))
     cond = ex["Cond"]
     foreach(k->cond[k]=tryparseparam.(cond[k]), keys(cond))
+    cond = cond |> DataFrame
+    
     ct = ex["CondTest"]
+    nct = mapreduce(length, max, values(ct))
+    foreach(k->begin
+        v = eltype(ct[k])==Any ? ct[k] : Any[ct[k]...]
+        l = length(v)
+
+        for i in eachindex(v)
+            if isnothing(v[i]) || isempty(v[i])
+                v[i] = missing
+            end
+        end
+        if endswith(k,"Index")
+            @warn "Change CondTest[$k]: 0-based to 1-based"
+            v.+=1            
+        end
+
+        if l < nct
+            @warn "Pad CondTest[$k] Length: $l to $nct"
+            append!( v, fill(missing, nct - l) )
+        end
+
+        ct[k] = v
+    end, keys(ct))
+    ct = ct |> DataFrame
+
+    ex["Cond"] = cond
+    ex["CondTest"] = ct
+
+    if "CondIndex" in names(ct)
+        ctc = transform(ct,:CondIndex => ByRow(i-> NamedTuple([f=>ismissing(i) ? missing : cond[i,f] for f in propertynames(cond)])) => AsTable)
+        ex["CondTestCond"] = ctc
+    end    
+
 end
